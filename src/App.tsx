bce10f98b36b7332fd27d728e71b36802c7308e6
unknown <khoshtaria.gio@gmail.com>
2024-07-20 13:36:40 +0400

    add defaultTheme and globalDtyling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react'
 
 import './App.css'
+import GlobalStyles from './components/utils/GlobalStyling'
 
 function App() {
 
 
   return (
     <>
+    <GlobalStyles/>
       gio
     </>
   )
---
+++ src/components/utils/GlobalStyling.tsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+import { defaultTheme } from "./defaulTheme";
+
+const GlobalStyles = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Antonio:wght@100..700&family=League+Spartan:wght@100..900&display=swap');
+
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
+    background-color: ${defaultTheme.colors.black};
+    font-family: ${defaultTheme.fonts.primary};
+    display: flex;
+    /* align-items: center; */
+    justify-content: center;
+    height: 100vh;
+
+}
+
+
+`;
+export default GlobalStyles;
